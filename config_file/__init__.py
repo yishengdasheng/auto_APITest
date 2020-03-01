56a4b3947f874ca292749f28c0822e0697b28458
yishengdasheng <44149989+yishengdasheng@users.noreply.github.com>
2020-03-01 18:05:30 +0800

    Initial commit

---
+++ config_file/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 _*-
+#!/usr/bin/python3
+
+#   author : YOYO
+#   time :  2020/3/1 14:03
+#   email : youyou.xu@enesource.com
+#   project_name :  test_read_mysql 
+#   file_name :  __init__.py.py
+#   function： 
---
+++ test_case/do_mysql.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 _*-
+#!/usr/bin/python3
+
+#   author : YOYO
+#   time :  2020/3/1 14:05
+#   email : youyou.xu@enesource.com
+#   project_name :  test_read_mysql 
+#   file_name :  do_mysql.py
+#   function： 连接数据库，并尝试从数据库获取数据
+
+
+class Cases:
+    def __init__(self):
+        pass
